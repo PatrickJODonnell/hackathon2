04b1004fccd67d41a1c253ea4879306419c30832
scoot <scondo@bearcognition.com>
2025-01-15 16:24:35 -0500

    dev/added infra necessities

--- main.py
+++ main.py
@@ -2,29 +2,42 @@ import requests
 from snowflake.snowpark.functions import udf
 from snowflake.snowpark.types import StringType
 
+
 def calculate_territorry(company: str):
-    url=f'https://noticeably-touching-cow.ngrok-free.app/territory'
-    result = requests.get(url, params={'company': company})
-    return (result['territory'])                    
+    url = f"https://noticeably-touching-cow.ngrok-free.app/territory"
+    result = requests.get(url, params={"company": company})
+    return result["territory"]
 
 
 def calculate_company_revenue(company: str, territory: str):
-    url=f'https://noticeably-touching-cow.ngrok-free.app/gather'
-    result = requests.get(url, params={'company': company, 'territory': territory})
-    return (result['company_revenue'])                    
+    url = f"https://noticeably-touching-cow.ngrok-free.app/gather"
+    result = requests.get(url, params={"company": company, "territory": territory})
+    return result["company_revenue"]
 
 
 def calculate_company_size(company: str, territory: str):
-    url=f'https://noticeably-touching-cow.ngrok-free.app/gather'
-    result = requests.get(url, params={'company': company, 'territory': territory})
-    return (result['company_size'])                    
+    url = f"https://noticeably-touching-cow.ngrok-free.app/gather"
+    result = requests.get(url, params={"company": company, "territory": territory})
+    return result["company_size"]
 
 
 def calculate_industry(company: str, territory: str):
-    url=f'https://noticeably-touching-cow.ngrok-free.app/gather'
-    result = requests.get(url, params={'company': company, 'territory': territory})
-    return (result['industry'])                    
+    url = f"https://noticeably-touching-cow.ngrok-free.app/gather"
+    result = requests.get(url, params={"company": company, "territory": territory})
+    return result["industry"]
 
 
+def main_territory(event: dict, context: dict) -> dict:
+    print(event)
 
 
+def main_calculate_company_rev(event: dict, context: dict) -> dict:
+    print(event)
+
+
+def main_calculate_company_size(event: dict, context: dict) -> dict:
+    print(event)
+
+
+def main_calculate_industry(event: dict, context: dict) -> dict:
+    print(event)
